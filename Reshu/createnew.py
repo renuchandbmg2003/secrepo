aff9cb6cfd57b14f630658c57474538ec4a2daca
Renu <renuchnadbmg2004@hmail.com>
2026-06-10 11:43:09 +0530

    sec commit

--- Reshu/createnew.py
+++ Reshu/createnew.py
@@ -5,3 +5,18 @@ if n%2==0:
     print(n,"is even")
 else:
     print(n,"is odd")
+
+
+print("-------------------------------------")
+print("Swapping")
+a=5
+b=10
+print("before swapping")
+print(a)
+print(b)
+print("after swapping")
+a=a+b
+b=a-b
+a=b-a
+print(a)
+print(b)
